1b8ed3e0e43244816b15393c758dcb69088cd3ac
Joseph Gaeddert <jgaedder@vt.edu>
2011-11-15 14:27:55 -0500

    wifi: adding interleaver, autotests

---
+++ autotest/interleaver_autotest.c
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2011 Joseph Gaeddert
+ * Copyright (c) 2011 Virginia Polytechnic Institute & State University
+ *
+ * This file is part of liquid.
+ *
+ * liquid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * liquid is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with liquid.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//
+// interleaver_autotest.c
+//
+// Test interleaver; data obtained from
+// Annex G in 1999 specification (Tables ...)
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <getopt.h>
+#include <time.h>
+
+#include <liquid/liquid.h>
+#include <fec.h>
+
+int main(int argc, char*argv[])
+{
+    // input messages
+    unsigned char msg_org[6] = {0xd1, 0xa1, 0x02, 0x3e, 0x70, 0x00};
+
+    // interleaved messages (tests)
+    unsigned char msg_test[6] = {0x94, 0xd0, 0x14, 0x83, 0x24, 0x94};
+
+    // options...
+    unsigned int ncbps = 48;    // number of coded bits per OFDM symbol
+    unsigned int nbpsc = 1;     // number of bits per subcarrier (modulation depth)
+
+    // interleave message
+    unsigned char msg_enc[6];
+    wifi_interleaver_encode_symbol(ncbps, nbpsc, msg_org, msg_enc);
+    if (count_bit_errors_array(msg_enc, msg_test, 6) > 0) {
+        fprintf(stderr,"fail: %s, encoding failure\n", __FILE__);
+        exit(1);
+    }
+
+    // de-interleave message
+    unsigned char msg_dec[6];
+    wifi_interleaver_decode_symbol(ncbps, nbpsc, msg_enc, msg_dec);
+    if (count_bit_errors_array(msg_dec, msg_org, 6) > 0) {
+        fprintf(stderr,"fail: %s, decoding failure\n", __FILE__);
+        exit(1);
+    }
+
+    return 0;
+}
+
---
+++ src/interleaver.c
@@ -0,0 +1,161 @@
+/*
+ * Copyright (c) 2011 Joseph Gaeddert
+ * Copyright (c) 2011 Virginia Polytechnic Institute & State University
+ *
+ * This file is part of liquid.
+ *
+ * liquid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * liquid is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with liquid.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//
+// 802.11a/g interleaver
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "liquid-802.11.internal.h"
+
+// intereleave one OFDM symbol
+//  _ncbps      :   number of coded bits per OFDM symbol
+//  _nbpsc      :   number of bits per subcarrier (modulation depth)
+//  _msg_dec    :   decoded message (de-iterleaved)
+//  _msg_enc    :   encoded message (interleaved)
+void wifi_interleaver_encode_symbol(unsigned int _ncbps,
+                                    unsigned int _nbpsc,
+                                    unsigned char * _msg_dec,
+                                    unsigned char * _msg_enc)
+{
+    // validate input
+    if (_ncbps % 8) {
+        fprintf(stderr,"error: wifi_interleaver_encode_symbol(), invalid ncbps\n");
+        exit(1);
+    }
+
+    // internal arrays
+    unsigned char msg_p0[_ncbps/8]; // first permutation
+
+    unsigned int k; // original
+    unsigned int i;
+    unsigned int j;
+
+    unsigned int s = (_nbpsc / 2) < 1 ? 1 : _nbpsc/2; // max( _nbpsc/2, 1 )
+
+    div_t d0;
+    div_t d1;
+    unsigned char bit;
+    
+    // outer permutation
+    for (k=0; k<_ncbps; k++) {
+        i = (_ncbps/16)*(k % 16) + (k/16);
+
+        d0 = div(k, 8);
+        d1 = div(i, 8);
+
+        bit = (_msg_dec[d0.quot] >> (8-d0.rem-1)) & 0x01;
+        msg_p0[d1.quot] |= bit << (8-d1.rem-1);
+    }
+
+    // inner permutation
+    for (i=0; i<_ncbps; i++) {
+        j = s*(i/s) + (i + _ncbps - ((16*i)/_ncbps) ) % s;
+
+        d0 = div(i, 8);
+        d1 = div(j, 8);
+
+        bit = (msg_p0[d0.quot] >> (8-d0.rem-1)) & 0x01;
+        _msg_enc[d1.quot] |= bit << (8-d1.rem-1);
+    }
+}
+
+// intereleave message
+//  _ncbps      :   number of coded bits per OFDM symbol
+//  _nbpsc      :   number of bits per subcarrier (modulation depth)
+//  _n          :   input messge length (bytes)
+//  _msg_dec    :   decoded message (de-iterleaved)
+//  _msg_enc    :   encoded message (interleaved)
+void wifi_interleaver_encode(unsigned int _ncbps,
+                             unsigned int _nbpsc,
+                             unsigned int _n,
+                             unsigned char * _msg_dec,
+                             unsigned char * _msg_enc)
+{
+}
+
+// de-intereleave one OFDM symbol
+//  _ncbps      :   number of coded bits per OFDM symbol
+//  _nbpsc      :   number of bits per subcarrier (modulation depth)
+//  _msg_enc    :   encoded message (interleaved)
+//  _msg_dec    :   decoded message (de-iterleaved)
+void wifi_interleaver_decode_symbol(unsigned int _ncbps,
+                                    unsigned int _nbpsc,
+                                    unsigned char * _msg_dec,
+                                    unsigned char * _msg_enc)
+{
+    // validate input
+    if (_ncbps % 8) {
+        fprintf(stderr,"error: wifi_interleaver_encode_symbol(), invalid ncbps\n");
+        exit(1);
+    }
+
+    // internal arrays
+    unsigned char msg_p0[_ncbps/8]; // first permutation
+
+    unsigned int k; // original
+    unsigned int i;
+    unsigned int j;
+
+    unsigned int s = (_nbpsc / 2) < 1 ? 1 : _nbpsc/2; // max( _nbpsc/2, 1 )
+
+    div_t d0;
+    div_t d1;
+    unsigned char bit;
+    
+    // inner permutation
+    for (i=0; i<_ncbps; i++) {
+        j = s*(i/s) + (i + _ncbps - ((16*i)/_ncbps) ) % s;
+
+        d0 = div(j, 8);
+        d1 = div(i, 8);
+
+        bit = (_msg_enc[d0.quot] >> (8-d0.rem-1)) & 0x01;
+        msg_p0[d1.quot] |= bit << (8-d1.rem-1);
+    }
+
+    // outer permutation
+    for (k=0; k<_ncbps; k++) {
+        i = (_ncbps/16)*(k % 16) + (k/16);
+
+        d0 = div(i, 8);
+        d1 = div(k, 8);
+
+        bit = (msg_p0[d0.quot] >> (8-d0.rem-1)) & 0x01;
+        _msg_dec[d1.quot] |= bit << (8-d1.rem-1);
+    }
+}
+
+// de-intereleave message
+//  _ncbps      :   number of coded bits per OFDM symbol
+//  _nbpsc      :   number of bits per subcarrier (modulation depth)
+//  _n          :   input messge length (bytes)
+//  _msg_enc    :   encoded message (interleaved)
+//  _msg_dec    :   decoded message (de-iterleaved)
+void wifi_interleaver_decode(unsigned int _ncbps,
+                             unsigned int _nbpsc,
+                             unsigned int _n,
+                             unsigned char * _msg_enc,
+                             unsigned char * _msg_dec)
+{
+}
+
