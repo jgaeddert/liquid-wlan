cc8a70a440f51778250ca91ff99bd6ac1fa1f3ac
Joseph Gaeddert <jgaedder@vt.edu>
2011-11-15 10:40:47 -0500

    sandbox: adding sandbox, initial data scrambler test

---
+++ sandbox/datascrambler_test.c
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2011 Joseph Gaeddert
+ * Copyright (c) 2011 Virginia Polytechnic Institute & State University
+ *
+ * This file is part of liquid.
+ *
+ * liquid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * liquid is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with liquid.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//
+// datascrambler_test.c
+//
+// Test data scrambler; should generate this sequence (127 bits) with
+// the 'all ones' inital state:
+//  00001110 11110010 11001001 00000010
+//  00100110 00101110 10110110 00001100
+//  11010100 11100111 10110100 00101010
+//  11111010 01010001 10111000 1111111
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <getopt.h>
+#include <time.h>
+
+#include <liquid/liquid.h>
+#include "liquid-802.11.h"
+
+int main(int argc, char*argv[])
+{
+    // create m-sequence generator
+    unsigned int m = 7;     // polynomial order
+    unsigned int g = 0x91;  // generator polynomial: x^7 + x^4 + 1 = 1001 0001
+    unsigned int a = 0x7f;  // initial state: .111 1111
+
+    // create
+    msequence ms = msequence_create(m, g, a);
+
+    unsigned int i;
+    for (i=0; i<127; i++) {
+        unsigned int bit = msequence_advance(ms);
+        printf("%1u", bit);
+
+        if ( ((i+1)%8) == 0 )
+            printf("\n");
+    }
+    printf("\n");
+
+    msequence_destroy(ms);
+
+
+    printf("done.\n");
+    return 0;
+}
+
