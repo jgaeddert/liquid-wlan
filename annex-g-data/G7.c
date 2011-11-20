213b0deaf04678b0583ca5bfe13103518bec6cd3
Joseph Gaeddert <jgaedder@vt.edu>
2011-11-20 18:05:45 -0500

    annex-g-data: fixing bugs (G8/G9 flipped), adding Table G.7 data

--- annex-g-data/G7.c
+++ annex-g-data/G7.c
@@ -19,11 +19,13 @@
  */
 
 // Table G.8: SIGNAL field bits after encoding
-// 1101 0001
-// 1010 0001
-// 0000 0010
-// 0011 1110
-// 0111 0000
-// 0000 0000
-unsigned char annexg_G8[6] = {0xd1, 0xa1, 0x02, 0x3e, 0x70, 0x00};
-
+//  0   1   RATE:R1         8   0   -               16  0   LENGTH (MSB)
+//  1   0   RATE:R1         9   0   -               17  0   PARITY
+//  2   1   RATE:R1         10  1   -               18  0   SIGNAL TAIL
+//  3   1   RATE:R1         11  1   -               19  0   SIGNAL TAIL
+//  4   0   [reserved]      12  0   -               20  0   SIGNAL TAIL
+//  5   0   LENGTH(LSB)     13  0   -               21  0   SIGNAL TAIL
+//  6   0   -               14  0   -               22  0   SIGNAL TAIL
+//  7   1   -               15  0   -               23  0   SIGNAL TAIL
+//      1011 0001               0011 0000               0000 0000
+unsigned char annexg_G7[3] = {0xb1, 0x30, 0x00};
