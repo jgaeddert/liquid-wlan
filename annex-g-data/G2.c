e273e562d25098fdcb97ca6f1987c60edbc0f2ac
Joseph Gaeddert <jgaedder@vt.edu>
2011-11-20 09:40:11 -0500

    annex-g-data: adding Table G2: freq. of short sequence

---
+++ annex-g-data/G2.c
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2011 Joseph Gaeddert
+ * Copyright (c) 2011 Virginia Polytechnic Institute & State University
+ *
+ * This file is part of liquid.
+ *
+ * liquid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * liquid is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with liquid.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Table G.2: Frequency domain of short sequence
+// NOTE : data are fft-shifted from table listing. Here the data are listed
+//        as the IFFT input such that the first element corresponds to the
+//        DC subcarrier.
+float complex annexg_G22[64] = {
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+     -1.4720 + -1.4720*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+     -1.4720 + -1.4720*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      1.4720 +  1.4720*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      1.4720 +  1.4720*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      1.4720 +  1.4720*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      1.4720 +  1.4720*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      1.4720 +  1.4720*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+     -1.4720 + -1.4720*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      1.4720 +  1.4720*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+     -1.4720 + -1.4720*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+     -1.4720 + -1.4720*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      1.4720 +  1.4720*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I,
+      0.0000 +  0.0000*_Complex_I};
+
