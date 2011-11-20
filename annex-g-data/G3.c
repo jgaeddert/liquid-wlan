fe822e250fe291267d2d2b77a5468ed11d24ac31
Joseph Gaeddert <jgaedder@vt.edu>
2011-11-20 09:46:15 -0500

    annex-g-data: adding Table G3: time-domain short sequence (one period)

---
+++ annex-g-data/G3.c
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2011 Joseph Gaeddert
+ * Copyright (c) 2011 Virginia Polytechnic Institute & State University
+ *
+ * This file is part of liquid.
+ *
+ * liquid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * liquid is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with liquid.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Table G.3: Time-domain short sequence (one period)
+float complex annexg_G3[64] = {
+      0.0460 +  0.0460*_Complex_I,
+     -0.1320 +  0.0020*_Complex_I,
+     -0.0130 + -0.0790*_Complex_I,
+      0.1430 + -0.0130*_Complex_I,
+      0.0920 +  0.0000*_Complex_I,
+      0.1430 + -0.0130*_Complex_I,
+     -0.0130 + -0.0790*_Complex_I,
+     -0.1320 +  0.0020*_Complex_I,
+      0.0460 +  0.0460*_Complex_I,
+      0.0020 + -0.1320*_Complex_I,
+     -0.0790 + -0.0130*_Complex_I,
+     -0.0130 +  0.1430*_Complex_I,
+      0.0000 +  0.0920*_Complex_I,
+     -0.0130 +  0.1430*_Complex_I,
+     -0.0790 + -0.0130*_Complex_I,
+      0.0020 + -0.1320*_Complex_I,
+      0.0460 +  0.0460*_Complex_I,
+     -0.1320 +  0.0020*_Complex_I,
+     -0.0130 + -0.0790*_Complex_I,
+      0.1430 + -0.0130*_Complex_I,
+      0.0920 +  0.0000*_Complex_I,
+      0.1430 + -0.0130*_Complex_I,
+     -0.0130 + -0.0790*_Complex_I,
+     -0.1320 +  0.0020*_Complex_I,
+      0.0460 +  0.0460*_Complex_I,
+      0.0020 + -0.1320*_Complex_I,
+     -0.0790 + -0.0130*_Complex_I,
+     -0.0130 +  0.1430*_Complex_I,
+      0.0000 +  0.0920*_Complex_I,
+     -0.0130 +  0.1430*_Complex_I,
+     -0.0790 + -0.0130*_Complex_I,
+      0.0020 + -0.1320*_Complex_I,
+      0.0460 +  0.0460*_Complex_I,
+     -0.1320 +  0.0020*_Complex_I,
+     -0.0130 + -0.0790*_Complex_I,
+      0.1430 + -0.0130*_Complex_I,
+      0.0920 +  0.0000*_Complex_I,
+      0.1430 + -0.0130*_Complex_I,
+     -0.0130 + -0.0790*_Complex_I,
+     -0.1320 +  0.0020*_Complex_I,
+      0.0460 +  0.0460*_Complex_I,
+      0.0020 + -0.1320*_Complex_I,
+     -0.0790 + -0.0130*_Complex_I,
+     -0.0130 +  0.1430*_Complex_I,
+      0.0000 +  0.0920*_Complex_I,
+     -0.0130 +  0.1430*_Complex_I,
+     -0.0790 + -0.0130*_Complex_I,
+      0.0020 + -0.1320*_Complex_I,
+      0.0460 +  0.0460*_Complex_I,
+     -0.1320 +  0.0020*_Complex_I,
+     -0.0130 + -0.0790*_Complex_I,
+      0.1430 + -0.0130*_Complex_I,
+      0.0920 +  0.0000*_Complex_I,
+      0.1430 + -0.0130*_Complex_I,
+     -0.0130 + -0.0790*_Complex_I,
+     -0.1320 +  0.0020*_Complex_I,
+      0.0460 +  0.0460*_Complex_I,
+      0.0020 + -0.1320*_Complex_I,
+     -0.0790 + -0.0130*_Complex_I,
+     -0.0130 +  0.1430*_Complex_I,
+      0.0000 +  0.0920*_Complex_I,
+     -0.0130 +  0.1430*_Complex_I,
+     -0.0790 + -0.0130*_Complex_I,
+      0.0020 + -0.1320*_Complex_I};
+
