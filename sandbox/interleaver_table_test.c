fc5e49376eb32e3482a9e923f212c49b3670fb8f
Joseph Gaeddert <jgaedder@vt.edu>
2011-12-01 10:47:36 -0500

    wlan_interleaver|sandbox: adding test for rate 36 M bits/s

--- sandbox/interleaver_table_test.c
+++ sandbox/interleaver_table_test.c
@@ -19,9 +19,9 @@
  */
 
 //
-// interleaver_test.c
+// interleaver_table_test.c
 //
-// Test interleaver; data obtained from
+// Test generating structured interleaver tables; data obtained from
 // Annex G in 1999 specification (Tables ...)
 //
 
@@ -32,10 +32,11 @@
 #include <getopt.h>
 #include <time.h>
 
-//#include <liquid/liquid.h>
-
 #include "liquid-wlan.internal.h"
     
+#include "annex-g-data/G18.c"
+#include "annex-g-data/G21.c"
+
 struct wlan_interleaver_tab_s {
     unsigned char p0;   // input byte index
     unsigned char p1;   // output byte index
@@ -51,13 +52,8 @@ void wlan_interleaver_gentab(unsigned int _rate,
 
 int main(int argc, char*argv[])
 {
-#if 0
-    // options...
-    unsigned int ncbps = 48;    // number of coded bits per OFDM symbol
-    unsigned int nbpsc = 1;     // number of bits per subcarrier (modulation depth)
-#else
-    unsigned int rate = WLANFRAME_RATE_6;   // primitive rate
-#endif
+    // option(s)
+    unsigned int rate = WLANFRAME_RATE_36;   // primitive rate
     
     // 
     unsigned int ncbps  = wlanframe_ratetab[rate].ncbps;   // number of coded bits per OFDM symbol
@@ -70,20 +66,40 @@ int main(int argc, char*argv[])
     // print table
     unsigned int i;
     for (i=0; i<ncbps; i++) {
-        printf("  %3u > %3u [mask = 0x%.2x, bit = 0x%.2x]\n",
+        printf("  %3u : %3u > %3u [mask = 0x%.2x, bit = 0x%.2x]\n",
+                i,
                 intlv[i].p0,
                 intlv[i].p1,
                 intlv[i].mask,
                 intlv[i].bit);
     }
 
-#if 0
+    // run interleaving test (only if rate is 36)
+    if (rate == WLANFRAME_RATE_36) {
+        // Table G.18: coded bits of first DATA symbol
+        unsigned char * msg_org = annexg_G18;
 
-    memset(msg_enc, 0x00, (ncbps/8)*sizeof(unsigned char));
-    for (i=0; i<ncbps; i++) {
-        msg_enc[ intlv[i].p0 ] |= (msg_dec[ intlv[i].p1 ] & intlv[i].mask ) ? intlv[i].bit : 0;
+        // Table G.21: interleaved bits of first DATA symbol
+        unsigned char * msg_test = annexg_G21;
+
+        // interleaver output
+        unsigned char msg_enc[24];
+
+        // clear encoded message buffer
+        memset(msg_enc, 0x00, 24*sizeof(unsigned char));
+
+        // run interleaver
+        for (i=0; i<ncbps; i++) {
+            msg_enc[ intlv[i].p0 ] |= (msg_org[ intlv[i].p1 ] & intlv[i].mask ) ? intlv[i].bit : 0;
+        }
+
+        // print results
+        printf("interleaved:\n");
+        for (i=0; i<24; i++)
+            printf("%3u : 0x%.2x > 0x%.2x (0x%.2x) %s\n", i, msg_org[i], msg_enc[i], msg_test[i], msg_enc[i] == msg_test[i] ? "" : "*");
+        printf("errors : %3u / %3u\n", count_bit_errors_array(msg_enc, msg_test, 24), ncbps);
+
     }
-#endif
 
     printf("done.\n");
     return 0;
