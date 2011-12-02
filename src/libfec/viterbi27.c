1141822b4a641cf61f57a50de0dc40a45f8fb96c
Joseph Gaeddert <jgaedder@vt.edu>
2011-12-02 11:29:49 -0500

    wlan|libfec: mangling names (appending 'wlan_') to prevent library mismatch

--- src/libfec/viterbi27.c
+++ src/libfec/viterbi27.c
@@ -33,42 +33,42 @@
 #include "liquid-wlan.internal.h"
 
 /* Create a new instance of a Viterbi decoder */
-void *create_viterbi27(int len){
-    return create_viterbi27_port(len);
+void *wlan_create_viterbi27(int len){
+    return wlan_create_viterbi27_port(len);
 }
 
-void set_viterbi27_polynomial(int polys[2]){
-    set_viterbi27_polynomial_port(polys);
+void wlan_set_viterbi27_polynomial(int polys[2]){
+    wlan_set_viterbi27_polynomial_port(polys);
 }
 
-/* Initialize Viterbi decoder for start of new frame */
-int init_viterbi27(void *p,int starting_state){
-    return init_viterbi27_port(p,starting_state);
+/* initialize Viterbi decoder for start of new frame */
+int wlan_init_viterbi27(void *p,int starting_state){
+    return wlan_init_viterbi27_port(p,starting_state);
 }
 
 /* Viterbi chainback */
-int chainback_viterbi27(
+int wlan_chainback_viterbi27(
     void *p,
     unsigned char *data, /* Decoded output data */
     unsigned int nbits, /* Number of data bits */
     unsigned int endstate){ /* Terminal encoder state */
 
-    return chainback_viterbi27_port(p,data,nbits,endstate);
+    return wlan_chainback_viterbi27_port(p,data,nbits,endstate);
 }
 
 /* Delete instance of a Viterbi decoder */
-void delete_viterbi27(void *p){
-    delete_viterbi27_port(p);
+void wlan_delete_viterbi27(void *p){
+    wlan_delete_viterbi27_port(p);
 }
 
 /* Update decoder with a block of demodulated symbols
  * Note that nbits is the number of decoded data bits, not the number
  * of symbols!
  */
-int update_viterbi27_blk(void *p,unsigned char syms[],int nbits){
+int wlan_update_viterbi27_blk(void *p,unsigned char syms[],int nbits){
     if(p == NULL)
         return -1;
 
-    update_viterbi27_blk_port(p,syms,nbits);
+    wlan_update_viterbi27_blk_port(p,syms,nbits);
     return 0;
 }
