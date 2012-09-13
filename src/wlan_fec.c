2019317b4bafd5264cdfc3e31e2bf9e46a71738b
Joseph D. Gaeddert <jgaedder@vt.edu>
2012-09-13 14:50:14 -0400

    fec: fixing bug with viterbi chainback (need to include tail bits)

--- src/wlan_fec.c
+++ src/wlan_fec.c
@@ -151,7 +151,7 @@ void wlan_fec_decode(unsigned int    _fec_scheme,
 
     // initialize encoder options
     unsigned int R                = wlanconv_fectab[_fec_scheme].R;
-    //unsigned int K              = wlanconv_fectab[_fec_scheme].K;
+    unsigned int K                = wlanconv_fectab[_fec_scheme].K;
     //const unsigned int * genpoly= wlanconv_fectab[_fec_scheme].genpoly;
     
     // puncturing options
@@ -219,7 +219,7 @@ void wlan_fec_decode(unsigned int    _fec_scheme,
     // run Viterbi decoder
     void * vp = wlan_create_viterbi27(num_enc_bits);
     wlan_init_viterbi27(vp,0);
-    wlan_update_viterbi27_blk(vp, enc_bits, 8*_dec_msg_len);
+    wlan_update_viterbi27_blk(vp, enc_bits, 8*_dec_msg_len + K - 1);
     wlan_chainback_viterbi27(vp, _msg_dec, num_enc_bits, 0);
     wlan_delete_viterbi27(vp);
 }
